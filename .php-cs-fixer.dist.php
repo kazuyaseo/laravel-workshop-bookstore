8026f5379a861e0d4075887bd82adc83aa9b5e6b
HirosuguTakeshita <h_takeshita@yumemi.co.jp>
2025-01-15 22:37:15 +0900

    fix: 🐛 php-cs-fixer の修正

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,12 +15,14 @@ $finder = PhpCsFixer\Finder::create()
 $config = new PhpCsFixer\Config();
 
 return $config
-//    ->setRiskyAllowed(true)  // コードの挙動が変わる可能性があるので検討後に設定する
+    ->setRiskyAllowed(true)
     ->setRules([
         // お気に入りの設定があればお好きに設定
         // @see https://cs.symfony.com/doc/rules/index.html
         '@Symfony' => true, // Symfony が公式で提供する Coding Standards https://symfony.com/doc/current/contributing/code/standards.html
         '@PhpCsFixer' => true,
         '@PSR12' => true, // PSR2 を拡張したもの
+
+        'declare_strict_types' => true, //
     ])
     ->setFinder($finder);
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,15 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_addition(): void
+    public function testAddition(): void
     {
         $a = 1;
         $b = 2;
